8ccdc761aac599c669cf039fd08e3a5e9511e20e
Jennifer <142938327+Jennifercruz23@users.noreply.github.com>
2025-08-24 10:32:30 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/hithighlighter/main.go
@@ -0,0 +1,23 @@
+// cmd/hithighlighter/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "hithighlighter/internal/hithighlighter"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := hithighlighter.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
